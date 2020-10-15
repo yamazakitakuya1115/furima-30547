9716c7b916253b8cbd157495b520e9ad8afe697f
yamazakitakuya1115 <yamazakitakuya1115@gmail.com>
2020-10-15 18:11:32 +0900

    devise導入〜新規登録機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+    binding.pry
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :first_kanji, :last_kanji, :first_kana, :last_kana, :birthday)
+  end
 end
