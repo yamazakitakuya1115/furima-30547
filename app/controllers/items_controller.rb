4521f87465045ead1233bba0eb08991c8ace2db7
yamazakitakuya1115 <yamazakitakuya1115@gmail.com>
2020-10-16 15:30:31 +0900

    アソシエーションとcreateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,19 @@
 class ItemsController < ApplicationController
-  
+
   def index
   end
 
   def new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :status_id, :delivery_id, :prefecture_id:, :days_id, :price_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   # 新規登録時のバリデーション
-  
   with_options presence: true do
     validates :nickname, :birthday
     validates :first_kanji, :last_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください' }
@@ -10,4 +10,6 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   end
+
+  has_many :items
 end
