a0726e9a921c9bd5e998542cbc56876b68a28c8f
yamazakitakuya1115 <yamazakitakuya1115@gmail.com>
2020-10-17 16:18:42 +0900

    単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+    after(:build) do |picture|
+      picture.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    name {'testcode洋服'}
+    explain {'testcode昨年買ったコートです。'}
+    category_id {1}
+    status_id {1}
+    delivery_id {1}
+    prefecture_id {1}
+    schedule_id {1}
+    price {10000}
   end
 end
