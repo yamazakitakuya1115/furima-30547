29cab2f070697e218d00ab95d6180c0f8aba6621
yamazakitakuya1115 <yamazakitakuya1115@gmail.com>
2020-10-19 16:11:58 +0900

    ordersテーブルに保存できた所まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,20 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to action: :index
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    validates :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
+  end
+
+  with_options numericality: { other_than: 0, message: 'を選択してください' } do
+    validates :prefecture_id
+  end
 end
