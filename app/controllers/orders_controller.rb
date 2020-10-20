69bb4c79c6f3bb2fc096bfdb3d6ba503e3a965d0
yamazakitakuya1115 <yamazakitakuya1115@gmail.com>
2020-10-20 09:48:07 +0900

    Formオブジェクト作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,30 +4,35 @@ class OrdersController < ApplicationController
   before_action :move_to_index, only: [:index, :create]
 
   def index
-    
+    @order_address = OrderAddress.new
   end
 
   def create
-    order = Order.create(order_params)
-    Address.create(address_params(order))
-    redirect_to root_path
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
 
-  def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
-  end
+  # def order_params
+  #   params.permit(:item_id).merge(user_id: current_user.id)
+  # end
 
-  def address_params(order)
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: order.id)
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def set_item
-    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def move_to_index
+    @item = Item.find(params[:item_id])
     if current_user.id == @item.user.id
       redirect_to root_path
     end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  with_options presence: true do
-    validates :postal_code, :city, :house_number, :phone_number
-  end
-
-  validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
-
 end
